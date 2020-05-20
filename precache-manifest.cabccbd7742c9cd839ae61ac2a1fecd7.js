d8f11bb3a44ab5f8b72fce479783f5b30d8da1ea
Viktor Bogutskii <info@siteograf.com>
2020-05-19 18:55:52 -0700

    Updates

---
+++ precache-manifest.cabccbd7742c9cd839ae61ac2a1fecd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cd91a81304dced0a281a795983d09a",
+    "url": "/react-2/index.html"
+  },
+  {
+    "revision": "0e240635e19730633391",
+    "url": "/react-2/static/css/main.cb4a4162.chunk.css"
+  },
+  {
+    "revision": "8b7ae0f76c7cb8398e2b",
+    "url": "/react-2/static/js/2.11e19211.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-2/static/js/2.11e19211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e240635e19730633391",
+    "url": "/react-2/static/js/main.e87cef4a.chunk.js"
+  },
+  {
+    "revision": "f2dde1c197f513cf7d93",
+    "url": "/react-2/static/js/runtime-main.57799c9a.js"
+  }
+]);
